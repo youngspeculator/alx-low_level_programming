ef65f5bad3acc71f6ba2cd2b5c22db2e6a4f400b
youngspeculator <jayypalll@gmail.com>
2022-09-14 07:00:43 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,9 +2,9 @@
 /**
  * main - prints the alphabet in lowercase then in uppercase,
  *		followed by a new line
- * only use the putchar function
- * All your code should be in the main function
- * You can only use putchar three times in your code
+ * only use the putchar function.
+ * All your code should be in the main function.
+ * You can only use putchar three times in your code.
  * Return: Always 0 (Success)
  */
 int main(void)

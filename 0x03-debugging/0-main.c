8375a074acb085966a67033c35b6f80f7563bdcf
youngspeculator <jayypalll@gmail.com>
2022-09-16 16:55:03 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests for a postitive_or_negative() function
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}

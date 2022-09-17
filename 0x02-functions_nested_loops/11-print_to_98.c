8ea053cbb96de2bab2ecdc3b6f292ca1e3ddc23d
youngspeculator <jayypalll@gmail.com>
2022-09-17 05:23:05 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,29 +10,13 @@
 
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n <= 98)
 	{
-		while (n > 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n--;
-		}
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
 	}
 	else
-	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
-		}
-	}
-	printf("\n");
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
 }

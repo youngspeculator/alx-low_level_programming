ce11a80f9e1b9e93956b1c25d9054309a3c3f4ac
youngspeculator <jayypalll@gmail.com>
2022-09-18 11:17:32 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}

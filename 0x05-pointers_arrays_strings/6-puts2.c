c63147c4a1ca1d28a1c14671b916cf557203d413
youngspeculator <jayypalll@gmail.com>
2022-09-19 13:55:58 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character, followed by a new line
+ * @str: input
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}

ee4e029418931e83e2a55eee14fb4a4d19768a69
youngspeculator <jayypalll@gmail.com>
2022-09-25 02:38:23 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @c: char pointer
+ * Return: char pointer
+ */
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] > 96 && c[i] < 123)
+			c[i] -= 32;
+		i++;
+	}
+	return (c);
+}

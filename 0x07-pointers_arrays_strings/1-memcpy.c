64b4123405b9d6a6d9583c0396913bcb904588c0
youngspeculator <jayypalll@gmail.com>
2022-09-26 04:35:19 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @src: source
+ * @dest: destination
+ * @n: bytes of memory to copy
+ * Return: pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}

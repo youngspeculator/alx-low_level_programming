b648d1d840888075eed46ab6b333ee2d7c0c27b8
youngspeculator <jayypalll@gmail.com>
2022-10-17 07:33:32 -0700

    This program will assign a random number to the variable n each time it is executed.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,24 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *main- This one assigns random number to variable n each time it is executed
- *	Complete the source code in order to print the last digit
+ * main - Assigns random number to variable n each time it is executed
+ * Complete the source code in order to print the last digit
  *	of the number stored in the variable n
- *source code was external
- *The variable n will store a different value every time you run this program
- *You don’t have to understand what rand, srand, and RAND_MAX do.
- *    Please do not touch this code
- *The output of the program should be:
- *     The string Last digit of, followed by
- *            n, followed by
- *     the string is, followed by
- *		if the last digit of n is greater than 5:
- *		   the string and is greater than 5
- *		if the last digit of n is 0: the string and is 0
- *		if the last digit of n is less than 6 and not 0:
- *		the string and is less than 6 and not 0
- *	followe by a new line
- *Return: Always 0 (Success)
+ * The variable n will store a different value every time you run this program
+ * You don’t have to understand what rand, srand, and RAND_MAX do.
+ * The output of the program should be:
+ * The string Last digit of, followed by n, followed by
+ * the string [is], followed by:
+ *	if the last digit of n>5:the string [and is greater than 5]
+ *	if the last digit of n==0:the string [and is 0]
+ *	if last digit of n<6 and !=0:the string[and is less than 6 and not 0]
+ *	followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -32,10 +27,10 @@ int main(void)
 	L = n % 10;
 
 	if (L > 5)
-		printf("%s %d is %d and is greater than 5\n", str, n, L);
+		printf("%s%d is %d and is greater than 5\n", str, n, L);
 	else if (L == 0)
-		printf("%s %d is %d and is 0\n", str, n, L);
+		printf("%s%d is %d and is 0\n", str, n, L);
 	else if (L < 6)
-		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
+		printf("%s%d is %d and is less than 6 and not 0\n", str, n, L);
 	return (0);
 }

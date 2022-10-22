3f93113722d36e856d7e389d4d57061483b05a53
youngspeculator <jayypalll@gmail.com>
2022-10-22 12:48:37 -0700

    a function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,31 +12,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i = 0, j, size = 0;
-	char *str;
+	int count1, count2, concat, i;
+	char *p;
 
-	if (s1 != NULL)
-		for (i = 0; s1[i] != '\0'; i++)
-			size++;
-	str = malloc(sizeof(char) * (size + 1));
-	if (str == NULL)
-		return (NULL);
-	if (s1 == NULL && s2 == NULL)
-	{
-		str[0] = '\0';
-		return (str);
-	}
-	if (s1 != NULL)
-		for (i = 0; s1[i] != '\0'; i++)
-			str[i] = s1[i];
 	if (s1 == NULL)
-		i = 0;
-	if (s2 != NULL)
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-			str[i] = s2[j];
-			i++;
-		}
-	str[size] = '\0';
-	return (str);
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (count1 = 0; s1[count1]; count1++)
+		;
+	for (count2 = 0; s2[count2]; count2++)
+		;
+	concat =  count1 + count2 + 1;
+	p = malloc(concat * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < concat; i++)
+		i < count1 ? (p[i] = s1[i]) : (p[i] = s2[i - count1]);
+	return (p);
 }

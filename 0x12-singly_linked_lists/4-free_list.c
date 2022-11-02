845a67b13bdfd49b84ed4cb3005dc2b23e262441
youngspeculator <jayypalll@gmail.com>
2022-11-01 22:12:32 -0700

    0x12. singly-linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: beginning of list
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}

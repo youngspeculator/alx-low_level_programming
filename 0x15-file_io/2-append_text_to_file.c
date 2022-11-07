1eee11bf4b3ee1f3790976db9855605ce0c40e84
youngspeculator <jayypalll@gmail.com>
2022-11-07 10:51:07 -0800

    File i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,18 +9,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int append_file, len, wr_stat;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	append_file = open(filename, O_WRONLY | O_APPEND);
-	if (append_file == -1)
+
+	if (text_content != NULL)
+	{	
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filenam, O_WRONLY | O_APPEND)
+	w = write(o, text_content, len);
+
+	if (0 == -1 || w == -1)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
-	for (len = 0; text_content[len]; len++)
-		;
-	wr_stat = write(append_file, text_content, len);
-	close(append_file);
-	return (wr_stat == -1 ? -1 : 1);
+
+	close(o);
+
+	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,9 +3,8 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcnt1.h>
 #include <unistd.h>
-
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

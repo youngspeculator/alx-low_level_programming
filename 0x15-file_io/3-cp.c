4385d922ae85cada280ddc077e909239b43c4d61
youngspeculator <jayypalll@gmail.com>
2022-11-07 23:32:38 -0800

    file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,72 +1,100 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * error_file - checks if file can be opened
- * @file_from: file_from
- * @file_to: file_to
- * @argv: arguments vector
- * Return: no return
+ * create_buffer - allocates 1024 bytes for a buffer
+ * @file: the name of the file buffer is storing chars for
+ *
+ * Return: a pointer to the newly-allocated buffer.
  */
-void error_file(int file_from, int file_to, char *argv[])
+char *create_buffer(char *file)
 {
-	if (file_form == -1)
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
 	{
-		dprint(STDEIRR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDEIRR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO,
+			"Error: Can I write to %s\n", file);
 		exit(99);
+	}
+	return (buffer);
+}
+
+/**
+ *close_file - closes file descriptors
+ * @fd: the file descriptor to be cleared
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't clode fd %d\n", fd);
+		exit(100);
 	}
 }
 
 /**
- * main - check the code
- * @argc: numer of arguments
- * @argv: arguments vector
- * Return: Always 0.
+ * main - writes a program that copies the content of a file to another file
+ * @argc: the number of arguments supplied to the program
+ * @argv: an array of pointers to the arguments
+ *
+ * Return: 0 on success
+ *
+ * description: if number of argument is not correct one, exit with code 97
+ * if file_from does not exist, or if you can't read, exit with code 98
+ * if you can't create or if write to file_to fails, exit with code 99
+ * if you can't close a file descriptor, exit with code 100
  */
 int main(int argc, char *argv[])
 {
-	int file_form, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	int from, to, r, w;
+	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to")
-			exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_t0\n");
+		exit(97);
 	}
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_form, file_to argv);
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(98);
+		}
 
-	nchars = 1024;
-	while (nchars == 1024)
-	{
-		nchars = read(file_form, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
-	}
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		r = read(from, buffer, 1024);
+		to  = open(argv[2], O_WRONLY | O_APPEND);
 
-	err_close = close(file_from);
-	if (err_close == 1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't clode fd %d\n", file_from);
-		exit(100);
-	}
+	} while (r > 0);
 
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Erro: Can't close fd %d\n", file_from);
-		exir(100);
-	}
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
 	return (0);
 }

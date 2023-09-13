c7bfa4b92173e8d93b92dfdcbbc81aa35d13921c
youngspeculator <jayypalll@gmail.com>
2023-09-13 05:50:15 -0700

    adding int index after error in compilation corrected

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - returns the index of element 1[...]
+ * for which the cmp function does not return 0.
+ * @array: array
+ * @size: the number of elements in the array array
+ * @cmp: a pointer to the function to be used to compare values
+ *
+ * If size <= 0, return -1
+ * Return: -1 if no element matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (array && cmp && size > 0)
+for (i = 0; i < size; i++)
+if (cmp(array[i]))
+return (i);
+return (-1);
+}

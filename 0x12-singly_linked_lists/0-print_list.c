769637a284c8af25d3472bf24d5b31a790348430
youngspeculator <jayypalll@gmail.com>
2023-09-21 05:58:00 -0700

    checkers did not work because of naming error: error corrected

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the structure
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+size_t e = 0;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+h = h->next;
+}
+
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+}
+e++;
+}
+return (e);
+}

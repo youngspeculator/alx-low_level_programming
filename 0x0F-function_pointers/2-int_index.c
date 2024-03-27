a4b956a02b20db0b91ef06029d24540901b36e1f
youngspeculator <jayypalll@gmail.com>
2024-03-27 18:07:01 +0300

    adding 2.c with new name for int variable

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - is a function that searches for an integer 
+ * int_index returns the index of the 1st element /
+ * for which the cmp function does not return 0. 
+ * @array: contents of the array
+ * @size: the number of elements in the array array
+ * @cmp: a pointer to the function to be used to compare values
+ *
+ * If size <= 0, return -1
+ * Return: -1 if no element matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int index_counter;
+
+/** iterates over the array elems & applies cmp function to each elem */ 
+/** if compare function succeeds for any elem it returns the index of that elem */
+
+if (array == 0 && cmp == 0 && size > 0)
+return (-1);
+
+for (index_counter = 0; index_counter < size; index_counter++)
+{
+if (cmp(array[index_counter]))
+return (index_counter);
+}
+
+}
+

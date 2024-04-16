d557817843940e4b8ecde45f47bd79ef8adb272b
youngspeculator <jayypalll@gmail.com>
2024-04-16 16:00:19 +0000

    committing variadic sum calc

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * bz - variadic lis
+ * @n: argument counter
+ * @sum: integer representing sum of ints
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list bz; /*declare argument pointer variable*/
+unsigned int cntr;
+int sum;
+
+va_start(bz, n); /*initialize the argument list*/
+
+sum = 0;
+for (cntr = 0; cntr < n; cntr++)
+sum += va_arg(bz, int); /*get the optional args*/
+
+va_end(bz); /*done using the arg pointer variable*/
+return (sum);
+}

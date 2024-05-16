be1a514c7856c159f4c406298bd9638f79e695e2
youngspeculator <jayypalll@gmail.com>
2024-05-16 04:10:25 -0700

    adding the header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/* reads a text file and prints it to the POSIX standard output */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* creates a file */ 
+int create_file(const char *fileame, char *text_content);
+
+/* appends text at the end of the file */
+itn append_text_to_file(const char *filename, char *text_context);
+
+/* putchar function */
+int _putchar(char c);
+
+#endif

dae0f9258eceb7e977270f88138b3be64a01b6fd
youngspeculator <jayypalll@gmail.com>
2024-05-16 06:27:55 -0700

    something's buggy, change it

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *fileame, char *text_content);
 
 /* appends text at the end of the file */
-itn append_text_to_file(const char *filename, char *text_context);
+int append_text_to_file(const char *filename, char *text_context);
 
 /* putchar function */
 int _putchar(char c);
